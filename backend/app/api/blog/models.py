8012d10f7e7f59fa6089810dafbbc4c503bb2380
PervDaniil <studentdan123.900@gmail.com>
2025-01-06 02:08:05 +0600

    Backend Post model defined

--- backend/app/api/blog/models.py
+++ backend/app/api/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    id = models.BigAutoField(primary_key = True)
+    image = models.ImageField(upload_to = 'blog/images/')
+    title = models.CharField(blank = False, max_length = 64)
+    description = models.CharField(blank = True, max_length=1024)
+    created_at = models.DateTimeField(auto_now_add = True)
+    
+    def __str__(self):
+        return f'Post {self.title} | {self.id}'
