2800feaea81b72851d5682413307b34be8f2095b
PervDaniil <studentdan123.900@gmail.com>
2025-01-06 00:43:33 +0600

    Blog page created and added its route

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './assets/css/root.css';
+import BlogPage from './pages/BlogPage.tsx';
 import HomePage from './pages/HomePage.tsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ export default function App() {
 		<Router>
 			<Routes>
 				<Route path='/' element={<HomePage />} />
+				<Route path='/blog' element={<BlogPage />} />
 			</Routes>
 		</Router>
 	);
