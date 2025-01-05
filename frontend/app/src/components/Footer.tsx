fdbbbd09c4ee12468a64abc0900042ea51699ca4
PervDaniil <studentdan123.900@gmail.com>
2025-01-06 00:17:15 +0600

    Structure of the project was improved

--- frontend/app/src/components/Footer.tsx
+++ frontend/app/src/components/Footer.tsx
@@ -2,9 +2,29 @@ import React from "react";
 
 export default function Footer() {
     return (
-        <footer className="flex-col w-full py-20 px-10 bg-green-500 text-white">
-            <div className="text-center">
-                <span>© 2025 Kyrgyzstan Blog. All Rights Reserved.</span>
+        <footer className="flex-col w-full py-20 px-10 bg-black text-white">
+            <div className="flex items-center justify-evenly">
+                <div className="flex flex-col flex-wrap text-gray-400">
+                    <p className="text-white">Навигация</p>
+                    <span><a>О нас</a></span>
+                    <span><a>Статьи</a></span>
+                    <span><a>Главная</a></span>
+                    <span><a>Галерея</a></span>
+                    <span><a>Контакты</a></span>
+                </div>
+                <div className="flex flex-col flex-wrap text-gray-400">
+                    <p className="text-white">Прочее</p>
+                    <span><a>Права пользования</a></span>
+                    <span><a>Политика конфиденциальности</a></span>
+                </div>
+                <div className="flex flex-col flex-wrap text-gray-400">
+                    <p className="text-white">Контакты</p>
+                    <span><a>Номер: +996 700 123 369</a></span>
+                    <span><a>Email: kg.blogtourism@email.com</a></span>
+                </div>
+            </div>
+            <div className="text-center text-gray-400 pt-20">
+                <span>&copy; 2025 Kyrgyzstan Blog. All Rights Reserved.</span>
             </div>
         </footer>
     );
--- frontend/app/src/components/Header.tsx
+++ frontend/app/src/components/Header.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function Header() {
     return (
         <header className="sticky top-0 w-full py-5 flex items-center justify-evenly bg-white bg-opacity-90 z-50">
-            <h5 className="font-bold text-2xl text-green-500">Кыргызстан Туризм</h5>
+            <h5 className="font-bold text-2xl text-black" style={{ fontFamily: 'Nexa' }}>Kg Tourism</h5>
             <nav className="flex gap-10 text-gray-500">
                 <a href="">Главная</a>
                 <a href="">О Кыргызстане</a>
@@ -13,7 +13,11 @@ export default function Header() {
                 <a href="">Природа</a>
                 <a href="">Контакты</a>
             </nav>
-            <button className="bg-green-500 rounded-md px-5 py-2 text-white">Поиск</button>
+            <button className="bg-black rounded-full p-4 text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                </svg>
+            </button>
         </header>
     )
 }
