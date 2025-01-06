8d643a2d33d5bf112ede16cb2a0676e163fbd49c
PervDaniil <studentdan123.900@gmail.com>
2025-01-07 02:37:49 +0600

    Backend CORS configurated

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -3,3 +3,8 @@ from pathlib import Path
 BASE_DIR_PATH = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'DJANGO SECRET KEY FOR PRODUCTION'
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
--- backend/app/core/settings.py
+++ backend/app/core/settings.py
@@ -6,7 +6,9 @@ SECRET_KEY = config.SECRET_KEY
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    *config.ALLOWED_HOSTS
+]
 
 
 APPS = [
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -45,6 +48,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+    'http://frontend:3000',
+]
+
 
 ROOT_URLCONF = 'core.urls'
 
